f7fe9152f693cfc4a0feda121a3ab1ee7b0e8728
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-04-28 16:38:45 +0300

    added PullableObject class and updated PlayerController for MiniGame3

--- Assets/Scripts/Player/PlayerControllerMiniGame3.cs
+++ Assets/Scripts/Player/PlayerControllerMiniGame3.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControllerMiniGame3 : MonoBehaviour
+namespace Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /**
+     * the third minigame is a makeover for mathew.
+     * the interactions are:
+     *          plucking eyebrows
+     *          combing hair
+     *          cutting nails - in the feet
+     *          
+     */
+    public class PlayerControllerMiniGame3 : PlayerControllerBase 
     {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
+
+        protected override void OnInteraction(InputAction.CallbackContext context)
+        {
+            
+        }
     }
 }
