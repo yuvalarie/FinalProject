eb083f8e814da1574e5f2874b898d2708502f76c
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-05-07 18:02:37 +0300

    created the poster sticker class

---
+++ Assets/Scripts/Objects/PosterSticker.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Objects
+{
+    public class PosterSticker : MonoBehaviour
+    {
+        [Tooltip("The ID MUST match the ID of the stickerID in the PosterObjectLibrary scriptable object.")]
+        [SerializeField] private int stickerID;
+        public int StickerID => stickerID;
+    }
+}
