ad752ded52a1c7816d56d4b7b1630d9a1d37c4ac
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-05-07 18:30:13 +0300

    added PosterData, PosterManager, and StickerEntry classes for managing poster stickers

---
+++ Assets/Scripts/Objects/Poster/PosterManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Objects.Poster
+{
+    public class PosterManager : MonoBehaviour
+    {
+        [SerializeField] private PosterData posterData;
+    }
+}
---
+++ Assets/Scripts/Objects/Poster/StickerEntry.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+namespace Objects.Poster
+{
+    [Serializable]
+    public class StickerEntry
+    {
+        public int id;
+        public Vector3 localPos;
+        public Quaternion localRot;
+        public Vector3 localScale;
+    }
+}
