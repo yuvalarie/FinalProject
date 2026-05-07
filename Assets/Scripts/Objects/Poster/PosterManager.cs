0f7b16d1a5d050482ba1745053e6f797e46ca063
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-05-07 18:31:56 +0300

    added SavePoster method to PosterManager for saving sticker data

--- Assets/Scripts/Objects/Poster/PosterManager.cs
+++ Assets/Scripts/Objects/Poster/PosterManager.cs
@@ -5,5 +5,25 @@ namespace Objects.Poster
     public class PosterManager : MonoBehaviour
     {
         [SerializeField] private PosterData posterData;
+        [SerializeField] private Transform posterContainer;
+        // [SerializeField] private PosterObjectLibrary posterObjectLibrary;
+
+        public void SavePoster()
+        {
+            posterData.ClearData();
+            foreach (Transform child in posterContainer)
+            {
+                var sticker = child.GetComponent<PosterSticker>();
+                if (sticker == null) continue;
+                var entry = new StickerEntry
+                {
+                    id = sticker.StickerID,
+                    localPos = child.localPosition,
+                    localRot = child.localRotation,
+                    localScale = child.localScale
+                };
+                posterData.placedStickers.Add(entry);
+            }
+        }
     }
 }
