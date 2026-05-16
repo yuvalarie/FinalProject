359aad7bd95ce08aca0300116c9cb325020df3b0
yuvalarie <yuvalarie@gmail.com>
2026-05-16 14:59:01 +0300

    added page2 transitions

---
+++ Assets/Scripts/Transitions/RowTransition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Transitions
+{
+    public class RowTransition : MonoBehaviour
+    {
+        [Header("Teleport Destination")]
+        [Tooltip("The exact Transform where the player will appear in the new row.")]
+        [SerializeField] public Transform destinationSpawn;
+
+        [Header("Perspective Settings")]
+        [Tooltip("The scale the player should instantly become when arriving at this row.")]
+        [SerializeField] public float targetScale = 1f;
+        [SerializeField] public int sortingOrder = 0;
+    }
+}
