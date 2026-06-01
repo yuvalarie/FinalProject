98973b999e5eda1cc25bfd622ac0ba0c4f25cd8e
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-06-01 18:59:04 +0300

    add volume controls and ambiance events to AudioManager and FMODEvents

--- Assets/Scripts/Managers/FMODEvents.cs
+++ Assets/Scripts/Managers/FMODEvents.cs
@@ -1,9 +1,13 @@
+using FMODUnity;
 using UnityEngine;
 
 namespace Managers
 {
     public class FMODEvents : MonoSingleton<FMODEvents>
     {
-        
+        [Header("Ambiance")]
+        public EventReference page1OfficeAmbiance;
+        public EventReference lettersInTubesAmbiance;
+        public EventReference windAmbiance;
     }
 }
