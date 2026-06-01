b16474fbb1aafecc3e1f92adc90cef0c57207194
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-06-01 19:05:42 +0300

    add audio playback functionality to LetterInTube class

--- Assets/Scripts/Objects/LetterInTube.cs
+++ Assets/Scripts/Objects/LetterInTube.cs
@@ -1,8 +1,33 @@
+using Audio;
+using FMOD.Studio;
+using Managers;
 using UnityEngine;
 
 namespace Objects
 {
     public class LetterInTube : MonoBehaviour
     {
+        private EventInstance _letterSoundInstance;
+        public void StartAudio()
+        {
+            if (_letterSoundInstance.isValid())
+            {
+                _letterSoundInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                _letterSoundInstance.release();
+            }
+            var pitchValue = Random.Range(-1f, 1f);
+            var volumeValue = Random.Range(-1f, 1f);
+            var reference = FMODEvents.Instance.lettersInTubesAmbiance;
+            _letterSoundInstance = AudioManager.Instance.CreateInstance(reference, ("LetterTubePitch", pitchValue), ("LetterTubeVolume", volumeValue));
+            _letterSoundInstance.start();
+        }
+
+        public void StopAudio()
+        {
+            if (_letterSoundInstance.isValid())
+            {
+                _letterSoundInstance.release();
+            }
+        }
     }
 }
