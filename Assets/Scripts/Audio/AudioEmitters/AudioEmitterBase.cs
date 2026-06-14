039443601d97016e064a6987bbff42dbf617f1e1
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-06-14 18:12:59 +0300

    Added Audio to hell portal in minigame 2

--- Assets/Scripts/Audio/AudioEmitters/AudioEmitterBase.cs
+++ Assets/Scripts/Audio/AudioEmitters/AudioEmitterBase.cs
@@ -1,5 +1,6 @@
 using FMOD.Studio;
 using FMODUnity;
+using Unity.VisualScripting;
 using UnityEngine;
 using STOP_MODE = FMOD.Studio.STOP_MODE;
 
@@ -9,6 +10,7 @@ namespace Audio.AudioEmitters
     {
         protected string AudioEventName;
         protected EventInstance AudioInstance;
+        private bool _isAudioInstanceInitialized;
 
         protected abstract void SetAudioEventName();
 
@@ -30,16 +32,54 @@ namespace Audio.AudioEmitters
 
         public virtual void StartAudio()
         {
-            var eventReference = FMODEvents.Instance.GetEventReferenceByName(AudioEventName);
-            AudioInstance = AudioManager.Instance.CreateInstance(eventReference);
-            AudioInstance.set3DAttributes(transform.position.To3DAttributes());
+            if(!_isAudioInstanceInitialized)
+            {
+                InitializeAudioInstance();
+            }
             AudioInstance.start();
         }
         
-        public virtual void StopAudio(STOP_MODE stopMode = STOP_MODE.ALLOWFADEOUT)
+        public virtual void StopAudio(STOP_MODE stopMode = STOP_MODE.IMMEDIATE)
         {
-            AudioInstance.stop(stopMode);
-            AudioInstance.release();
+            if(_isAudioInstanceInitialized)
+            {
+                AudioInstance.stop(stopMode);
+            }
+        }
+
+        public virtual void ResumeAudio()
+        {
+            if(!_isAudioInstanceInitialized)
+            {
+                StartAudio();
+                return;
+            }
+            AudioInstance.setPaused(false);
+        }
+        
+        public virtual void PauseAudio()
+        {
+            if(_isAudioInstanceInitialized)
+            {
+                AudioInstance.setPaused(true);
+            }
+        }
+        
+        private void  InitializeAudioInstance()
+        {
+            var eventReference = FMODEvents.Instance.GetEventReferenceByName(AudioEventName);
+            AudioInstance = AudioManager.Instance.CreateInstance(eventReference);
+            AudioInstance.set3DAttributes(transform.position.To3DAttributes());
+            _isAudioInstanceInitialized = true;
+        }
+        
+        private void OnDestroy()
+        {
+            if(_isAudioInstanceInitialized)
+            {
+                AudioInstance.release();
+                _isAudioInstanceInitialized = false;
+            }
         }
     }
 }
