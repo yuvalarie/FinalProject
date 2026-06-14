7caccbcdaad5fca9568f8a66af3cf4243f12e88c
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-06-14 16:15:18 +0300

    moved audio scripts for better orginization

---
+++ Assets/Scripts/Audio/AudioCallers/AudioCallerBase.cs
@@ -0,0 +1,45 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
+
+namespace Audio
+{
+    public abstract class AudioCallerBase : MonoBehaviour
+    {
+        protected string AudioEventName;
+        protected EventInstance AudioInstance;
+
+        protected abstract void SetAudioEventName();
+
+        protected void OnEnable()
+        {
+            SetAudioEventName();
+        }
+
+        public virtual void PlayAudio()
+        {
+            if (string.IsNullOrEmpty(AudioEventName))
+            {
+                Debug.LogError($"Audio event name is not set for {gameObject.name}");
+                return;
+            }
+            var eventReference = FMODEvents.Instance.GetEventReferenceByName(AudioEventName);
+            AudioManager.Instance.PlayOneShot(eventReference, transform.position);
+        }
+
+        public virtual void StartAudio()
+        {
+            var eventReference = FMODEvents.Instance.GetEventReferenceByName(AudioEventName);
+            AudioInstance = AudioManager.Instance.CreateInstance(eventReference);
+            AudioInstance.set3DAttributes(transform.position.To3DAttributes());
+            AudioInstance.start();
+        }
+        
+        public virtual void StopAudio(STOP_MODE stopMode = STOP_MODE.ALLOWFADEOUT)
+        {
+            AudioInstance.stop(stopMode);
+            AudioInstance.release();
+        }
+    }
+}
--- Assets/Scripts/Audio/AudioCallers/FactoryDoorAudioCaller.cs
+++ Assets/Scripts/Audio/AudioCallers/FactoryDoorAudioCaller.cs
@@ -2,7 +2,7 @@ namespace Audio
 {
     public class FactoryDoorAudioCaller : AudioCallerBase
     {
-        private protected override void SetAudioEventName()
+        protected override void SetAudioEventName()
         {
             AudioEventName = AudioEventNames.FactoryDoorOpen;
         }
