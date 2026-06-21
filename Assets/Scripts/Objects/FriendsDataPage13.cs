889e83fee87d78a85325924d4c1249f75522ff5f
yuvalarie <yuvalarie@gmail.com>
2026-06-21 18:17:59 +0300

    add connections between friends scene to page 13

---
+++ Assets/Scripts/Objects/FriendsDataPage13.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Objects.Poster
+{
+    [CreateAssetMenu(fileName = "FriendsDataPage13", menuName = "Page13/Friend Data")]
+    public class FriendsDataPage13 : ScriptableObject
+    {
+        public int id;
+        public GameObject frame2Object;
+        public GameObject frame4Object;
+    }
+}
