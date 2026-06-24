773f4c8b4c7f113e723bb38f1cc3806c5515c725
yuvalarie <yuvalarie@gmail.com>
2026-06-24 08:49:32 +0300

    page 13

---
+++ Assets/Scripts/Player/Pages/PlayerControllerPage13.cs
@@ -0,0 +1,21 @@
+﻿using Objects.Poster;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player.Pages
+{
+    public class PlayerControllerPage13 : PlayerControllerBase
+    {
+        [Header("Poster Settings")]
+        //[SerializeField] PosterManager
+        
+        private bool atPosterCollider1;
+        private bool atPosterCollider2;
+        private bool atPosterCollider3;
+        
+        protected override void OnInteraction(InputAction.CallbackContext context)
+        {
+            
+        }
+    }
+}
