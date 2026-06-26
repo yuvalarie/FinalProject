82730739129d7ceafcd890044ef22f9243405494
yuvalarie <yuvalarie@gmail.com>
2026-06-26 13:10:25 +0300

    mini game 3

--- Assets/Scripts/Helpers/MiniGame3PimpleAnimator.cs
+++ Assets/Scripts/Helpers/MiniGame3PimpleAnimator.cs
@@ -7,11 +7,18 @@ namespace Objects
 {
     public class MiniGame3PimpleAnimator : MonoBehaviour
     {
+        private static readonly int Pop = Animator.StringToHash("Pop");
         [SerializeField] private Animator pimple1Animator;
         [SerializeField] private Animator pimple2Animator;
         [SerializeField] private Animator pimple3Animator;
         [SerializeField] private Animator pimple4Animator;
         [SerializeField] private Animator pimple5Animator;
+
+        [SerializeField] private float wait1;
+        [SerializeField] private float wait2;
+        [SerializeField] private float wait3;
+        [SerializeField] private float wait4;
+        [SerializeField] private float wait5;
 
         private void Start()
         {
@@ -20,16 +27,16 @@ namespace Objects
 
         private IEnumerator PimpleCoroutine()
         {
-            yield return new WaitForSeconds(3f);
-            pimple1Animator.SetTrigger("Pop");
-            yield return new WaitForSeconds(10f);
-            pimple2Animator.SetTrigger("Pop");
-            yield return new WaitForSeconds(5f);
-            pimple3Animator.SetTrigger("Pop");
-            yield return new WaitForSeconds(7f);
-            pimple4Animator.SetTrigger("Pop");
-            yield return new WaitForSeconds(2f);
-            pimple5Animator.SetTrigger("Pop");
+            yield return new WaitForSeconds(wait1);
+            pimple1Animator.SetTrigger(Pop);
+            yield return new WaitForSeconds(wait2);
+            pimple2Animator.SetTrigger(Pop);
+            yield return new WaitForSeconds(wait3);
+            pimple3Animator.SetTrigger(Pop);
+            yield return new WaitForSeconds(wait4);
+            pimple4Animator.SetTrigger(Pop);
+            yield return new WaitForSeconds(wait5);
+            pimple5Animator.SetTrigger(Pop);
         }
     }
 }
