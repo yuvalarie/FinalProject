4d031ad829a5f7d2e1b268221ec14a8aa5825038
yuvalarie <yuvalarie@gmail.com>
2026-07-01 12:13:11 +0300

    page 15

---
+++ Assets/Scripts/Player/Pages/PlayerControllerPage15.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using Managers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player.Pages
+{
+    public class PlayerControllerPage15 : PlayerControllerBase
+    {
+        [Header("Sequence Settings")] 
+        [SerializeField] private GameObject screen0;
+        [SerializeField] private GameObject screen1;
+        [SerializeField] private GameObject screen2;
+        [SerializeField] private GameObject screen3;
+        [SerializeField] private GameObject screen4;
+        
+        [Header("Trigger Settings")]
+        [SerializeField] private Collider2D startCollider;
+        
+        private bool isSequenceActive = false;
+        private bool hasSequenceStarted = false;
+        private bool isAnimating = false;
+        private bool axisInUse = false;
+        private int interactionCount;
+
+        protected override void Start()
+        {
+            base.Start();
+            SceneLoader.Instance.PreloadScene(nextSceneName);
+        }
+        
+        protected override void OnInteraction(InputAction.CallbackContext context)
+        {
+        }
+        
+        protected override void HandleMovement()
+        {
+            if (isSequenceActive)
+            {
+                Rb.linearVelocity = Vector2.zero;
+                return;
+            }
+            
+            base.HandleMovement();
+        }
+        
+        private void Update()
+        {
+            if (!isSequenceActive) return;
+
+            if (Mathf.Abs(MoveInput.x) < 0.1f)
+            {
+                axisInUse = false;
+            }
+
+            if (isAnimating || axisInUse) return;
+
+            if (MoveInput.x > 0.5f && interactionCount < 4)
+            {
+                axisInUse = true;
+                interactionCount++;
+                TransitionToState(interactionCount, true);
+            }
+            else if (MoveInput.x < -0.5f && interactionCount > 0)
+            {
+                axisInUse = true;
+                if(interactionCount > 1) interactionCount--;
+                TransitionToState(interactionCount, false);
+            }
+        }
+        
+        private void TransitionToState(int targetState, bool isMovingForward)
+        {
+            screen0.SetActive(false);
+            screen1.SetActive(false);
+            screen2.SetActive(false);
+            screen3.SetActive(false);
+            screen4.SetActive(false);
+            isAnimating = true;
+
+            switch (targetState)
+            {
+                case 1:
+                    screen1.SetActive(true);
+                    break;
+                case 2:
+                    screen2.SetActive(true);
+                    break;
+                case 3:
+                    screen3.SetActive(true);
+                    break;
+                case 4:
+                    screen4.SetActive(true);
+                    isSequenceActive = false;
+                    break;
+            }
+
+            isAnimating = false;
+        }
+
+        protected override void OnTriggerEnter2D(Collider2D other)
+        {
+            base.OnTriggerEnter2D(other);
+            if (other == startCollider && !hasSequenceStarted)
+            {
+                hasSequenceStarted = true;
+                isSequenceActive = true;
+                interactionCount = 1;
+                axisInUse = true;
+                TransitionToState(interactionCount, true);
+            }
+        }
+    }
+}
