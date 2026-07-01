718b667f460367efdf9411ed8db2b40c9f0adf7e
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-07-01 21:38:18 +0300

    added flipping to the friends when they roam

--- Assets/Scripts/Objects/HellPortal.cs
+++ Assets/Scripts/Objects/HellPortal.cs
@@ -1,5 +1,6 @@
 using System;
 using Audio.AudioEmitters;
+using FMOD.Studio;
 using Npc;
 using UnityEngine;
 
@@ -24,7 +25,7 @@ namespace Objects
             if (_inFlightCount == 1)
             {
                 // Only play the sound when the first friend is sucked in, to avoid overlapping sounds.
-                audioEmitter.ResumeAudio();
+                audioEmitter.StartAudio();
                 // audioEmitter.StartAudio(("Indicator", 0f)); // 0 == start, which will fade out the music
             }
             friend.GetThrown(transform.position, OnFriendArrived);
@@ -47,7 +48,7 @@ namespace Objects
             // This method can be called after the animation finishes to hide the portal.
             // You can use an animation event at the end of the animation to call this method.
             // audioEmitter.SetParameter("Indicator", 1f); // 1 == end, which will fade in the music
-            audioEmitter.StopAudio(); // 1 == end, which will fade in the music
+            audioEmitter.StopAudio(STOP_MODE.ALLOWFADEOUT);
             gameObject.SetActive(false);
             // we might want to set a dispersing animation here instead of just hiding it, but for now this is fine.
         }
