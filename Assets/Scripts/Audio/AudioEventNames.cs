67c9825ad3b3f72fe3a724e6476114744565d1db
liel callaf <liel.marx-callaf@mail.huji.ac.il>
2026-07-09 04:58:14 +0300

    added audio to minigame 4_2

--- Assets/Scripts/Audio/AudioEventNames.cs
+++ Assets/Scripts/Audio/AudioEventNames.cs
@@ -136,6 +136,11 @@ namespace Audio
         public const string PrinterButtonClick = "Printer Button Click";
         public const string PrintPoster = "Print Poster";
         
+        // minigame 5
+        public const string BallotInserted = "Ballot Inserted";
+        public const string BallotGrabbed = "Ballot Grabbed";
+        public const string AshtrayPickup = "Ashtray Pickup";
+
         //Breakdown
         public const string BreakdownShaker = "Breakdown Shaker";
         public const string BreakdownBabyWalking = "Breakdown Baby Walking";
